6ae78e329a3f12e838baa0920d511de082eae4d8
Timur Kabaev <timur_kabaev@epam.com>
2022-02-11 12:04:54 +0300

    initial commit

---
+++ src/main/java/com/kabaev/testing/AmigoscodeSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.kabaev.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeSoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeSoftwareTestingApplication.class, args);
+	}
+
+}
